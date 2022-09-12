b0af4033606f74106678cb7a70709e655f0735b4
RobinKohrs <robin.kohrs@gmx.de>
2022-09-12 15:32:33 +0200

    add output to gitignore

--- js/downloadStromErzeugung.js
+++ js/downloadStromErzeugung.js
@@ -2,7 +2,8 @@ import fs from "fs";
 import * as d3 from "d3";
 import _ from "lodash";
 import node_fetch from "node-fetch";
-import { group } from "console";
+import { createObjectCsvWriter } from "csv-writer";
+
 global.fetch = node_fetch;
 
 // Formatting Date functions
@@ -27,46 +28,33 @@ export default async function donwloadStromErzeugung() {
   const YEAR = makeYear(TODAY);
   const DATE_FORMATTED = `${YEAR}-${MONTH}-${DAY}`;
 
-  const url = `https://transparency.apg.at/transparency-api/api/v1/Download/AGPT/German/M15/2022-08-29T000000/${DATE_FORMATTED}T000000/AGPT_2022-08-28T22_00_00Z_${DATE_FORMATTED}T22_00_00Z_60M_de_${DATE_FORMATTED}T15_40_08Z.csv`;
+  let filename =
+    outDir + "/" + DATE_FORMATTED.toString().replace(/-/g, "_") + ".csv";
+
+  const url = `https://transparency.apg.at/transparency-api/api/v1/Download/AGPT/German/M15/2022-08-01T000000/${DATE_FORMATTED}T000000/AGPT_2022-08-01T22_00_00Z_${DATE_FORMATTED}T22_00_00Z_60M_de_${DATE_FORMATTED}T15_40_08Z.csv`;
+
+  // if file already exists do not fetch it!
+  if (fs.existsSync(filename)) {
+    console.log(`file for ${DATE_FORMATTED} already exists`);
+    return;
+  }
 
   // Fetch data
-  let data = await d3.dsv(";", url);
-
-  // format data
-  data = data.map((e) => {
-    return {
-      ...e,
-      date: e["Zeit von [CET/CEST]"].slice(0, 10),
-    };
-  });
+  let data;
+  try {
+    data = await d3.dsv(";", url);
+    // add date to each row
+    data = data.map((e) => {
+      return {
+        ...e,
+        date: e["Zeit von [CET/CEST]"].slice(0, 10),
+      };
+    });
+  } catch (e) {
+    console.log(`error `, e);
+  }
 
   let groupedByDate = _.groupBy(data, (d) => d.date);
-
-  //
-  // GROUPED BY DATE
-  //
-  // {
-  //   dateN: [
-  //     {
-  //       time1:
-  //       gas: ...
-  //     },
-  //     {
-  //       time2:
-  //       gas: ...
-  //     }
-  //   ],
-  //   dateN+1: [
-  //     {
-  //       time1:
-  //       gas: ...
-  //     },
-  //     {
-  //       time2:
-  //       gas: ...
-  //     }
-  //   ]
-  // }
 
   // the keys are the dates
   let keys = Object.keys(groupedByDate);
@@ -104,6 +92,8 @@ export default async function donwloadStromErzeugung() {
     }, {});
 
   res.forEach((d, i) => {
+    if (i === res.length - 1) {
+    }
     let date = d.date[0];
     let types = Object.keys(d);
     types.forEach((t, j) => {
@@ -112,14 +102,28 @@ export default async function donwloadStromErzeugung() {
         dates[date][t] = val;
       } else {
         let val = d[t].reduce((acc, curr) => {
-          curr = parseInt(curr.replace(",", "."));
+          curr = parseFloat(curr.replace(",", "."));
           return acc + curr;
         }, 0);
         dates[date][t] = val;
       }
     });
   });
-  console.log(`dates`, dates);
+
+  let final = Object.keys(dates).map((k) => dates[k]);
+  let r0 = Object.keys(final[0]).map((k) => {
+    return {
+      id: k,
+      title: k,
+    };
+  });
+
+  const csvWriter = createObjectCsvWriter({
+    path: filename,
+    header: r0,
+  });
+
+  csvWriter.writeRecords(final).then(() => console.log("written"));
 }
 
 donwloadStromErzeugung();
