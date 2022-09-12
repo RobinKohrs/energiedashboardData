d9c713f7c0932aa2f6c22106f2b8c53870fdafaa
RobinKohrs <robin.kohrs@gmx.de>
2022-09-12 11:24:48 +0200

    sum values for one day

--- js/downloadStromErzeugung.js
+++ js/downloadStromErzeugung.js
@@ -41,18 +41,50 @@ export default async function donwloadStromErzeugung() {
   });
 
   let groupedByDate = _.groupBy(data, (d) => d.date);
+
+  //
+  // GROUPED BY DATE
+  //
+  // {
+  //   dateN: [
+  //     {
+  //       time1:
+  //       gas: ...
+  //     },
+  //     {
+  //       time2:
+  //       gas: ...
+  //     }
+  //   ],
+  //   dateN+1: [
+  //     {
+  //       time1:
+  //       gas: ...
+  //     },
+  //     {
+  //       time2:
+  //       gas: ...
+  //     }
+  //   ]
+  // }
+
+  // the keys are the dates
   let keys = Object.keys(groupedByDate);
 
   let res = keys.map((d) => {
+    // d is a single date...
+
+    // valsOneDay is an array of Objects of 15 Minute intervals on that day
     let valsOneDay = groupedByDate[d];
 
-    // make the base
+    // get enery-types from the first fifteen minutes object
     let types = Object.keys(valsOneDay[0]);
     let dateObj = types.reduce((acc, curr) => {
       acc[curr] = [];
       return acc;
     }, {});
 
+    // for each timestamp add the values to the dateObj-Object
     valsOneDay.forEach((d, i) => {
       let typesOneTime = Object.keys(d);
       typesOneTime.forEach((t) => {
@@ -63,6 +95,31 @@ export default async function donwloadStromErzeugung() {
 
     return dateObj;
   });
+
+  let dates = res
+    .map((e) => e.date[0])
+    .reduce((acc, curr) => {
+      acc[curr] = {};
+      return acc;
+    }, {});
+
+  res.forEach((d, i) => {
+    let date = d.date[0];
+    let types = Object.keys(d);
+    types.forEach((t, j) => {
+      if (j == 0 || j == 1 || j === types.length - 1) {
+        let val = d[t][0];
+        dates[date][t] = val;
+      } else {
+        let val = d[t].reduce((acc, curr) => {
+          curr = parseInt(curr.replace(",", "."));
+          return acc + curr;
+        }, 0);
+        dates[date][t] = val;
+      }
+    });
+  });
+  console.log(`dates`, dates);
 }
 
 donwloadStromErzeugung();
